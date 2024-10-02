cf71ee547756a50ead4354439604a6c7bf4ae2ff
Miguel Rios <mriogut600@g.educaand.es>
2024-10-02 16:13:51 +0200

    Se ha añadido getTics()

--- src/main/java/es/iesjandula/ReaktorIssuesServer/rest/RestHandlerIssuesServer.java
+++ src/main/java/es/iesjandula/ReaktorIssuesServer/rest/RestHandlerIssuesServer.java
@@ -1,8 +1,13 @@
 package es.iesjandula.ReaktorIssuesServer.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.iesjandula.ReaktorIssuesServer.error.IssuesServerError;
+import es.iesjandula.ReaktorIssuesServer.repository.ITicRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -11,4 +16,22 @@ import lombok.extern.slf4j.Slf4j;
 public class RestHandlerIssuesServer
 {
 	
+	@Autowired
+	private ITicRepository ticRepository ;
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/getTics")
+	public ResponseEntity<?> obtenerIncidenciasTic()
+	{
+		try
+		{
+			return ResponseEntity.ok().body(this.ticRepository.getTics()) ;
+		}
+		catch (Exception exception)
+		{
+			String message = "No se ha podido obtener la lista de Tics de la Base de Datos";
+			log.error(message, exception);
+			IssuesServerError serverError= new IssuesServerError(0, message, exception);
+			return ResponseEntity.status(500).body(serverError.getMapError());
+		}
+	}
 }
