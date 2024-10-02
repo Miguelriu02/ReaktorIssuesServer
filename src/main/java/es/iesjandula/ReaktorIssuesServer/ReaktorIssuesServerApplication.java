249bf7f494ed8b51ddb7bad5987e6f8b61e3ffc8
Miguel Rios <mriogut600@g.educaand.es>
2024-10-02 17:47:59 +0200

    Proyecto Finalizado+

--- src/main/java/es/iesjandula/ReaktorIssuesServer/ReaktorIssuesServerApplication.java
+++ src/main/java/es/iesjandula/ReaktorIssuesServer/ReaktorIssuesServerApplication.java
@@ -10,15 +10,9 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = "es.iesjandula")
 public class ReaktorIssuesServerApplication
 {
-
-<<<<<<< HEAD
-	public static void main(String[] args)
-	{
-=======
 	 // El main es el punto de entrada de la aplicación, aquí es donde comienza la ejecución.
     // SpringApplication.run() arranca el contexto de Spring, inicializando todos los beans y configuraciones necesarias.
 	public static void main(String[] args) {
->>>>>>> 9f41d3d0c12c19c7a9db7f526baedddea66aa315
 		SpringApplication.run(ReaktorIssuesServerApplication.class, args);
 	}
 
